47e9e37850f35f4e60e89628e100f9aa2a632e53
Yrahcaz7 <zachary.shive@gmail.com>
2022-08-26 12:27:24 -0400

    vertical centering & things

--- js/game.js
+++ js/game.js
@@ -1,3 +1,11 @@
 var game = {
-	points: 1,
+	points: 0,
+	tab: "main",
 };
+
+const loop = setInterval(() => {
+	let main = document.getElementById("main");
+	if (game.tab == "main") {
+		main.innerHTML = "test<h2>TEST</h2>test<br>test<br><br>test<h1>TEST</h1>test<h5>TEST</h5>test";
+	};
+}, 30);
