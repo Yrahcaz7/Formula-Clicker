26591557b0326f78b7d319f9232c1223e505ef8f
Yrahcaz7 <zachary.shive@gmail.com>
2022-09-14 09:27:11 -0400

    bugfix

--- js/infinity.js
+++ js/infinity.js
@@ -9,7 +9,7 @@ function prestige() {
 	game.points = 0;
 	game.pointBest = 0;
 	game.pointTotal = 0;
-	if (game.infinity_milestones[2]) game.clicks *= 0.5;
+	if (game.infinity.milestones[2]) game.clicks *= 0.5;
 	else game.clicks = 0;
 	game.tab = "main";
 	game.unlocks = [];
